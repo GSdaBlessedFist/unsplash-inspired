0910468cff0501abf3ba3368195c7d73b54e1b0f
GSdaBlessedFist <gsdablessedfist@gmail.com>
2022-12-12 17:40:14 -0600

    1) header completed...NEXT::Moving on to layout

---
+++ pages/_document.tsx
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <title>Unsplash-like site</title>
+        <link href="https://fonts.googleapis.com/css2?family=PT+Mono&display=swap" rel="stylesheet"/>
+      </Head>
+      
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
